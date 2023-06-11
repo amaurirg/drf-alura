62078bfeab1b8d82b1ad81cf7b37b1b2f3d8f554
Amauri Rossetti Giovani <amaurirg@terra.com.br>
2023-06-11 19:38:16 -0300

    Added clientes app

--- setup/urls.py
+++ setup/urls.py
@@ -17,19 +17,21 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from escola.views import AlunosViewSet, CursosViewSet, MatriculasViewSet, ListaMatriculasAluno, \
-    ListaMatriculadosPorCurso
+from clientes.views import ClientesViewSet
+from escola.views import (
+    AlunosViewSet, CursosViewSet, MatriculasViewSet, ListaMatriculasAluno, ListaMatriculadosPorCurso
+)
 from rest_framework.routers import DefaultRouter
-
 
 router = DefaultRouter()
 router.register("alunos", AlunosViewSet, basename="Alunos")
 router.register("cursos", CursosViewSet, basename="Cursos")
 router.register("matriculas", MatriculasViewSet, basename="Matriculas")
+router.register('clientes', ClientesViewSet, basename="Clientes")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('aluno/<int:pk>/matriculas/', ListaMatriculasAluno.as_view()),
-    path('curso/<int:pk>/matriculas/', ListaMatriculadosPorCurso.as_view())
+    path('curso/<int:pk>/matriculas/', ListaMatriculadosPorCurso.as_view()),
 ]
